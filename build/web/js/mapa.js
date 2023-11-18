2f86302cbfddcb4048dae8531ef62b2a3164603a
Nahum Josael Ramirez Lopez <148998254+MYXIEMPRESA@users.noreply.github.com>
2023-11-18 17:34:19 -0600

    mapa arreglado Nahum

--- build/web/js/mapa.js
+++ build/web/js/mapa.js
@@ -1,42 +1,91 @@
+var map;
+
 function initMap() {
-  // Coordenadas de la Ciudad de México
-  var ciudadDeMexicoBounds = {
-    north: 19.6415,
-    south: 19.1635,
-    east: -98.9346,
-    west: -99.3545
-  };
+    var ciudadDeMexicoBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(19.1635, -99.3545),
+        new google.maps.LatLng(19.6415, -98.9346)
+    );
 
-  // Crear un nuevo mapa y asociarlo con el elemento div con el id "map"
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 19.4326, lng: -99.1332 }, // Coordenadas centradas en la Ciudad de México
-    zoom: 10, // Nivel de zoom inicial
-    restriction: {
-      latLngBounds: ciudadDeMexicoBounds,
-      strictBounds: false
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 19.4326, lng: -99.1332 },
+        zoom: 10,
+        restriction: {
+            latLngBounds: ciudadDeMexicoBounds,
+            strictBounds: false
+        }
+    });
+
+    getLocation();
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition, showError);
+    } else {
+        document.getElementById("demo").innerHTML = "Geolocalización no es compatible con este navegador.";
     }
-  });
+}
 
-  // Restringir el área visible del mapa a la Ciudad de México
-  map.addListener('dragend', function() {
-    if (ciudadDeMexicoBounds.contains(map.getCenter())) return;
+function showPosition(position) {
+    // Commenting out the following line will prevent displaying latitude and longitude
+    // document.getElementById("demo").innerHTML = "Latitud: " + position.coords.latitude + "<br>Longitud: " + position.coords.longitude;
 
-    // Si el centro del mapa está fuera de los límites, ajustarlo de vuelta
-    var c = map.getCenter(),
-        x = c.lng(),
-        y = c.lat(),
-        maxX = ciudadDeMexicoBounds.east,
-        maxY = ciudadDeMexicoBounds.north,
-        minX = ciudadDeMexicoBounds.west,
-        minY = ciudadDeMexicoBounds.south;
+    map.setCenter({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    });
 
-    if (x < minX) x = minX;
-    if (x > maxX) x = maxX;
-    if (y < minY) y = minY;
-    if (y > maxY) y = maxY;
+    var marker = new google.maps.Marker({
+        position: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        },
+        map: map,
+        title: 'Ubicación en Tiempo Real'
+    });
+}
 
-    map.setCenter(new google.maps.LatLng(y, x));
-  });
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById("demo").innerHTML = "Usuario denegó la solicitud de geolocalización.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById("demo").innerHTML = "Información de ubicación no disponible.";
+            break;
+        case error.TIMEOUT:
+            document.getElementById("demo").innerHTML = "Tiempo de espera agotado para la solicitud de geolocalización.";
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById("demo").innerHTML = "Ocurrió un error desconocido.";
+            break;
+    }
+}
 
-  // ... Resto de tu código de inicialización del mapa
+function generateRoute() {
+    var destination = document.getElementById("destination").value;
+
+    if (destination.trim() === "") {
+        alert("Por favor, ingresa una ubicación válida.");
+        return;
+    }
+
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+
+    directionsRenderer.setMap(map);
+
+    var request = {
+        origin: new google.maps.LatLng(map.center.lat(), map.center.lng()),
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+    };
+
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(result);
+        } else {
+            alert("No se pudo calcular la ruta. Asegúrate de ingresar una ubicación válida.");
+        }
+    });
 }
